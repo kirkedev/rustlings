6945e5252c61ec4a325e70e5fcc1ac03b3d4fc19
Andrew Kirkegaard <r3volver@gmail.com>
2023-03-12 14:58:18 +0000

    Complete hashmaps

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -25,23 +23,57 @@ struct Team {
     goals_conceded: u8,
 }
 
+fn get_teams(result: &str) -> (Team, Team) {
+    let v: Vec<&str> = result.split(',').collect();
+    let team_1_name = v[0].to_string();
+    let team_1_score: u8 = v[2].parse().unwrap();
+    let team_2_name = v[1].to_string();
+    let team_2_score: u8 = v[3].parse().unwrap();
+
+    let winner = Team {
+        name: team_1_name, 
+        goals_scored: team_1_score, 
+        goals_conceded: team_2_score
+    };
+
+    let loser = Team {
+        name: team_2_name, 
+        goals_scored: team_2_score, 
+        goals_conceded: team_1_score
+    };
+
+    return (winner, loser);
+}
+
 fn build_scores_table(results: String) -> HashMap<String, Team> {
     // The name of the team is the key and its associated struct is the value.
     let mut scores: HashMap<String, Team> = HashMap::new();
 
-    for r in results.lines() {
-        let v: Vec<&str> = r.split(',').collect();
-        let team_1_name = v[0].to_string();
-        let team_1_score: u8 = v[2].parse().unwrap();
-        let team_2_name = v[1].to_string();
-        let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
+    for line in results.lines() {
+        let (winner, loser) = get_teams(line);
+
+        // Populate the scores table with details extracted from the
         // current line. Keep in mind that goals scored by team_1
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+       
+        if let Some(score) = scores.get_mut(&winner.name) {
+            score.goals_scored += winner.goals_scored;
+            score.goals_conceded += winner.goals_conceded;
+        } else {
+            scores.insert(winner.name.clone(), winner);
+        }
+
+        if let Some(score) = scores.get_mut(&loser.name) {
+            score.goals_scored += loser.goals_scored;
+            score.goals_conceded += loser.goals_conceded;
+        } else {
+            scores.insert(loser.name.clone(), loser);
+        }
     }
-    scores
+
+    return scores;
 }
 
 #[cfg(test)]
